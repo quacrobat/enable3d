27f265938d8c1d92360ea968a53f66ed57e600aa
Yannick <20306025+yandeu@users.noreply.github.com>
2020-02-25 20:24:01 +0100

    Build threeGraphics bundle

---
+++ webpack/webpack.threeGraphics.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+/**
+ * Makes the minified bundle
+ */
+module.exports = (env, argv) => {
+  return {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: path.resolve(__dirname, '../packages/threeGraphics/src/index.ts'),
+    output: {
+      filename: `enable3d.threeGraphics@${argv.packageVersion}.min.js`,
+      path: path.resolve(__dirname, `${argv.path}`),
+      library: 'ENABLE3D',
+      libraryTarget: 'umd'
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader'
+        }
+      ]
+    }
+  }
+}
