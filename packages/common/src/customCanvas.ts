7de40ac22170a58c27a029d71a6c8eef62efe944
Yannick <20306025+yandeu@users.noreply.github.com>
2020-02-25 16:48:17 +0100

    .

--- packages/common/src/customCanvas.ts
+++ packages/common/src/customCanvas.ts
@@ -7,9 +7,7 @@
 // copied from https://phaser.io/examples/v3/view/game-config/custom-webgl-canvas
 // copied from https://github.com/mrdoob/three.js/blob/dev/src/renderers/WebGLRenderer.js
 
-import { WEBGL, OrbitControls } from '@enable3d/three-wrapper/src/examples'
-
-console.log(OrbitControls)
+import { WEBGL } from '@enable3d/three-wrapper/src/examples'
 
 interface CustomCanvasConfig {
   antialias?: boolean
