1c06246acd9bbe3d50a1a74aee53fbbc06803f46
yandeu <20306025+yandeu@users.noreply.github.com>
2020-03-01 00:27:01 +0100

    Cleaned up scenes

--- packages/dev/src/scenes/mainScene.ts
+++ packages/dev/src/scenes/mainScene.ts
@@ -1,33 +1,18 @@
-import { Object3D, Scene3D, ExtendedObject3D } from '@enable3d/phaser-extension'
+import { Scene3D, Object3D, ExtendedObject3D } from '@enable3d/phaser-extension'
 
 export default class MainScene extends Scene3D {
-  sphere: Object3D
-  hero: ExtendedObject3D
-  robot: ExtendedObject3D
-  keys: any
-  gameOver: boolean
-  playerCanJump: boolean
-
   constructor() {
     super({ key: 'MainScene' })
   }
 
   init() {
     this.requestThirdDimension()
-    delete this.hero
-    delete this.robot
-    this.gameOver = false
-    this.playerCanJump = true
   }
 
   create() {
     this.accessThirdDimension()
     this.third.warpSpeed()
-    this.third.haveSomeFun(50)
   }
 
-  update(delta: number, time: number) {
-    this.third.physics.update(delta)
-    this.third.physics.updateDebugger()
-  }
+  update() {}
 }
